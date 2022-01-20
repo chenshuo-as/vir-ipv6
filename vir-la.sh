4b8a2b14152abcaa7a90b7f80c4340b452d1dc8d
chenshuo-as <44311021+chenshuo-as@users.noreply.github.com>
2022-01-20 22:05:19 +0800

    Update vir-la.sh

--- vir-la.sh
+++ vir-la.sh
@@ -6,7 +6,7 @@ echo "net.ipv6.conf.lo.disable_ipv6 = 0" >> /etc/sysctl.conf
 sysctl -p
 sleep 1
 
-sed -i '$a nameserver 2001:4860:4860::8888' /etc/resolv.conf
+#sed -i '$a nameserver 2001:4860:4860::8888' /etc/resolv.conf
 
 echo "auto he-ipv6" >> /etc/network/interfaces
 echo "iface he-ipv6 inet6 v4tunnel" >> /etc/network/interfaces
@@ -16,6 +16,9 @@ echo "endpoint 66.220.18.42" >> /etc/network/interfaces
 echo "local 107.175.61.23" >> /etc/network/interfaces
 echo "ttl 255" >> /etc/network/interfaces
 echo "gateway 2001:470:c:6be::1" >> /etc/network/interfaces
+
+echo "precedence  ::ffff:0:0/96   100" >> /etc/gai.conf
+
 echo -e "ipv6添加完成,请重启系统..."
 sleep 2
 #/etc/init.d/networking restart
